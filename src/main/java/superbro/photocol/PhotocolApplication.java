8c55228863666269ec286dcc8246644d5e3ccaae
Ivan Kolokolov <delocksuperbro@gmail.com>
2019-12-31 23:07:29 +0700

    init hello

---
+++ src/main/java/superbro/photocol/PhotocolApplication.java
@@ -0,0 +1,13 @@
+package superbro.photocol;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotocolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotocolApplication.class, args);
+    }
+}
