9e98621610b38f2f356cfd5c1430f00d1c772e12
Ivan Kolokolov <delocksuperbro@gmail.com>
2020-01-01 13:44:14 +0700

    index page

--- src/main/java/superbro/photocol/controller/IndexController.java
+++ src/main/java/superbro/photocol/controller/IndexController.java
@@ -1,13 +1,14 @@
 package superbro.photocol.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index(){
-        return "Hello!";
+        return "index";
     }
 }
