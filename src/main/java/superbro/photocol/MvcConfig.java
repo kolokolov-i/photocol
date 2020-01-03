89da85781afc5b341264bff4fc30884a5d884f31
superBro <delocksuperbro@gmail.com>
2020-01-04 01:38:16 +0700

    thymeleaf added

--- src/main/java/superbro/photocol/MvcConfig.java
+++ src/main/java/superbro/photocol/MvcConfig.java
@@ -9,5 +9,6 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
+        registry.addViewController("/hello").setViewName("hello");
     }
 }
